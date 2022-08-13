3bdce3a050e327c86afee39b5e5a0eeebcee9b4a
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-12 21:03:00 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -48,7 +48,7 @@ const App: React.FC = () => {
             <Route path="/farms">
               <Farms />
             </Route>
-            <Route path="/trucks">
+            <Route path="/pools">
               <Farms tokenMode />
             </Route>
             {/* <Route path="/pools"> */}
