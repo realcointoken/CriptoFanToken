0936ff05db39644962d5d0af80adbbe76f4b3ba8
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-13 23:19:09 -0300

    Update FarmCard.tsx

--- src/views/Farms/components/FarmCard/FarmCard.tsx
+++ src/views/Farms/components/FarmCard/FarmCard.tsx
@@ -120,7 +120,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
     : '-'
 
   const lpLabel = farm.lpSymbol
-  const earnLabel = 'CriptoFan'
+  const earnLabel = 'CryptoFan'
   const farmAPY =
     farm.apy &&
     farm.apy.times(new BigNumber(100)).toNumber().toLocaleString(undefined, {
@@ -132,7 +132,7 @@ const FarmCard: React.FC<FarmCardProps> = ({ farm, removed, cakePrice, bnbPrice,
 
   return (
     <FCard>
-      {farm.tokenSymbol === 'CryptoFanFan' && <StyledCardAccent />}
+      {farm.tokenSymbol === 'CryptoFan' && <StyledCardAccent />}
       <CardHeading
         lpLabel={lpLabel}
         multiplier={farm.multiplier}
