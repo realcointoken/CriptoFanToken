f87b034b0bb6616cc99cd29a376c1886bec894e7
ufogreys <45027262+ufogreys@users.noreply.github.com>
2022-08-13 22:19:31 -0300

    Update index.tsx

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -25,7 +25,7 @@ const Menu = (props) => {
       setLang={setSelectedLanguage}
       cakePriceUsd={cakePriceUsd.toNumber()}
       links={config}
-      priceLink="https://www.coingecko.com/en/coins/CriptoFan"
+      priceLink="https://www.coingecko.com/en/coins/CryptoFan"
       {...props}
     />
   )
